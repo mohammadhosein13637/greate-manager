248902248f267bf8588c593a96730ae6bd356464
mohammadhosein13637 <mohammadhosein.rezazadeh@outlook.com>
2025-08-26 13:57:00 +0330

    Project , Task Factories and seeders added

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,8 @@ class Project extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectFactory> */
     use HasFactory;
+
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
 }
